d5ffe4706200cfada283848d6f5126c3ff321fb4
Nahuel Palacios <nahuelmp1998@gmail.com>
2025-03-25 16:47:25 -0300

    added get_character_by_id route and function

--- controller/character.py
+++ controller/character.py
@@ -5,10 +5,18 @@ from model.character import Character
 router = APIRouter()
 
 
-@router.get("/character/getAll")
+@router.get("/character/getAll", tags=["Characters"])
 def get_all_characters():
     session = get_session()
     characters = session.query(Character).all()
     requested_attributes = ["id", "name", "height", "mass", "birth_year", "eye_color"]
     characters_formatted = [{attr: getattr(char, attr)} for char in characters for attr in requested_attributes]
     return characters_formatted
+
+@router.get("/character/get/{id}", tags=["Characters"])
+def get_character_by_id(id: int):
+    session = get_session()
+    character = session.query(Character).filter(Character.id == id).first()
+    return character
+    
+
