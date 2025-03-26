42cea5728921ae0850da4f2fba8c9b8cc8aee9df
Nahuel Palacios <nahuelmp1998@gmail.com>
2025-03-26 13:08:22 -0300

    updated

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-"""
-Documentation used
-    -https://docs.sqlalchemy.org/en/20/orm/quickstart.html#declare-models
-    -https://docs.sqlalchemy.org/en/20/tutorial/data_update.html
-"""
-
 from fastapi import FastAPI
 from config.lifespan import lifespan
-from controllers.character import router as character_router
+from controllers.character_controller import router as character_router
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(character_router) 
